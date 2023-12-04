771a8847ab5abf53a3855778ac05117cca7a261b
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-04 16:09:20 +0100

    Day4

---
+++ src/Day4/Game.cs
@@ -0,0 +1,27 @@
+﻿namespace Day4;
+
+public class Game
+{
+    private IEnumerable<int> WinningNumbers { get; }
+    private IEnumerable<int> PlayerNumbers { get; }
+    public  int CopyCount { get; private set; }
+
+    public Game(IEnumerable<int> winningNumbers, IEnumerable<int> playerNumbers)
+    {
+        WinningNumbers = winningNumbers;
+        PlayerNumbers = playerNumbers;
+        CopyCount = 1;
+    }
+
+    public int GetPoints()
+    {
+        var matchingNumber = WinningNumbers.Intersect(PlayerNumbers).ToList();
+        return matchingNumber.Count < 2 ? matchingNumber.Count : (int)Math.Pow(2, matchingNumber.Count - 1);
+    }
+    public int GetMatching() => WinningNumbers.Intersect(PlayerNumbers).Count();
+
+    public void AddCopies(int newCopies)
+    {
+        CopyCount += newCopies;
+    }
+}
---
+++ src/Day4/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day4;
+
+const string splitCharacter = ": ";
+const string numbersSplit = " | ";
+const char numbersSeparate = ' ';
+
+var inputLines = File.ReadAllLines("input.txt");
+
+var games = inputLines.Select(ParseGame).ToArray();
+var totalPoints = games.Select(item => item.GetPoints()).Sum();
+
+Console.WriteLine($"first: {totalPoints}");
+
+
+for (var index = 0; index < games.Length; index++)
+{
+    var currentGame = games[index];
+    var currentGameCopyCount = games[index].CopyCount;
+    var currentGameValue = currentGame.GetMatching();
+
+    for (var copyIndex = index + 1; copyIndex <= index + currentGameValue && copyIndex < games.Length; copyIndex++)
+    {
+        games[copyIndex].AddCopies(currentGameCopyCount);
+    }
+}
+
+var totalNumberOfGames = games.Select(item => item.CopyCount).Sum();
+Console.WriteLine($"second: {totalNumberOfGames}");
+
+return;
+
+Game ParseGame(string input)
+{
+    var gameData = input.Split(splitCharacter)[1].Split(numbersSplit);
+    var winningNumbers = gameData[0].Split(numbersSeparate).Where(item => !string.IsNullOrWhiteSpace(item)).Select(int.Parse);
+    var playerNumbers = gameData[1].Split(numbersSeparate).Where(item => !string.IsNullOrWhiteSpace(item)).Select(int.Parse);
+
+    return new Game(winningNumbers, playerNumbers);
+}
+
