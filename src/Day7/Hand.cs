b48af505a20321d5ec84b97f7aded114eba04894
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-07 10:48:04 +0100

    Day7

---
+++ src/Day7/Hand.cs
@@ -0,0 +1,113 @@
+﻿namespace Day7;
+
+public class Hand : IComparable<Hand>
+{
+    private int[] Cards { get; }
+    public int Bid { get; }
+    private CardCombo Combo { get; }
+
+    public Hand(int[] cards, int bid, bool useJoker)
+    {
+        Cards = cards;
+        Bid = bid;
+        Combo = GetCombo(cards, useJoker);
+    }
+
+    private static CardCombo GetCombo(int[] cards, bool userJoker)
+    {
+        var numberOfCards = new Dictionary<int, int>();
+        foreach (var card in cards)
+        {
+            if (numberOfCards.ContainsKey(card))
+            {
+                numberOfCards[card]++;
+            }
+            else
+            {
+                numberOfCards[card] = 1;
+            }
+        }
+
+        return userJoker ? CreateComboWithJoker(numberOfCards) : CreateCombo(numberOfCards);
+    }
+
+    private static CardCombo CreateCombo(Dictionary<int, int> numberOfCards)
+    {
+        return numberOfCards.Count switch
+        {
+            5 => CardCombo.HighCard,
+            4 => CardCombo.OnePair,
+            3 when numberOfCards.Any(item => item.Value == 3) => CardCombo.ThreeOfAKind,
+            3 when numberOfCards.MaxBy(item => item.Value).Value == 2 => CardCombo.TwoPair,
+            2 when numberOfCards.Any(item => item.Value == 3) => CardCombo.FullHouse,
+            2 when numberOfCards.Any(item => item.Value == 4) => CardCombo.FourOfAKind,
+            1 => CardCombo.FiveOfAKind,
+            _ => throw new Exception("unknown card combo")
+        };
+    }
+
+    private static CardCombo CreateComboWithJoker(Dictionary<int, int> numberOfCards)
+    {
+        var hasJoker = numberOfCards.TryGetValue(1, out var jokerCount);
+        if (!hasJoker)
+        {
+            return CreateCombo(numberOfCards);
+        }
+
+        var bestCombo = CardCombo.HighCard;
+        for (var substitute = 2; substitute < 14; substitute++)
+        {
+            var substituted = new Dictionary<int, int>(numberOfCards);
+            substituted.Remove(1);
+            if (substituted.ContainsKey(substitute))
+            {
+                substituted[substitute] += jokerCount;
+            }
+            else
+            {
+                substituted[substitute] = jokerCount;
+            }
+
+            var combo = CreateCombo(substituted);
+            if (combo > bestCombo)
+            {
+                bestCombo = combo;
+            }
+        }
+
+        return bestCombo;
+    }
+
+    public int CompareTo(Hand? other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        if (Combo < other.Combo)
+        {
+            return -1;
+        }
+
+        if (Combo > other.Combo)
+        {
+            return 1;
+        }
+
+        for (var i = 0; i < Cards.Length; i++)
+        {
+            if (Cards[i] < other.Cards[i])
+            {
+                return -1;
+            }
+
+            if (Cards[i] > other.Cards[i])
+            {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ src/Day7/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Day7;
+
+var inputLines = File.ReadAllLines("input.txt");
+var hands = CreateHands(inputLines, false).ToArray();
+var stopWatch = new Stopwatch();
+
+stopWatch.Start();
+Array.Sort(hands);
+long result = 0;
+for (var i = 0; i < hands.Length; i++)
+{
+    result += hands[i].Bid * (i + 1);
+}
+
+stopWatch.Stop();
+Console.WriteLine($"first: {result} - {stopWatch.Elapsed.Minutes}:{stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+
+var handsWithJokers = CreateHands(inputLines, true).ToArray();
+stopWatch.Restart();
+stopWatch.Start();
+Array.Sort(handsWithJokers);
+result = 0;
+for (var i = 0; i < handsWithJokers.Length; i++)
+{
+    result += handsWithJokers[i].Bid * (i + 1);
+}
+
+stopWatch.Stop();
+Console.WriteLine($"second: {result} - {stopWatch.Elapsed.Minutes}:{stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+return;
+
+IEnumerable<Hand> CreateHands(IEnumerable<string> input, bool useJoker)
+{
+    foreach (var line in input)
+    {
+        var split = line.Split(" ");
+        var cards = split[0].Select(item => useJoker ? ParseCardWithJoker(item): ParseCard(item)).ToArray();
+        var bid = int.Parse(split[1]);
+        yield return new Hand(cards, bid, useJoker);
+    }
+}
+
+int ParseCard(char value)
+{
+    return value switch
+    {
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => int.Parse(value.ToString())
+    };
+}
+int ParseCardWithJoker(char value)
+{
+    return value switch
+    {
+        'T' => 10,
+        'J' => 1,
+        'Q' => 11,
+        'K' => 12,
+        'A' => 13,
+        _ => int.Parse(value.ToString())
+    };
+}
