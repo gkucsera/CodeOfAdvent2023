6bf3bea50dad1ea6e99854e14209d7086628ed96
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-09 11:56:38 +0100

    Day8

---
+++ src/Day8/Day8/Map.cs
@@ -0,0 +1,45 @@
+﻿namespace Day8;
+
+public class Map
+{
+    private readonly Dictionary<string, MapEntry> _mapEntries;
+    public Map(IEnumerable<MapEntry> mapEntries)
+    {
+        _mapEntries = mapEntries.ToDictionary(item => item.Code, item => item);
+    }
+
+    public int CountMoves(string start, string end, string moves)
+    {
+        var result = 0;
+        var move = 0;
+        var mapEntry = _mapEntries[start];
+
+        while (mapEntry.Code != end)
+        {
+            var direction = moves[move];
+            mapEntry = direction == 'R' ? _mapEntries[mapEntry.Right] : _mapEntries[mapEntry.Left];
+            result++;
+            move++;
+            move %= moves.Length;
+        }
+
+        return result;
+    }
+    public int CountMultipleMoves(char start, char end, string moves)
+    {
+        var result = 0;
+        var move = 0;
+        var mapEntries = _mapEntries.Where(item => item.Key.EndsWith(start));
+
+        while (mapEntry.Code != end)
+        {
+            var direction = moves[move];
+            mapEntry = direction == 'R' ? _mapEntries[mapEntry.Right] : _mapEntries[mapEntry.Left];
+            result++;
+            move++;
+            move %= moves.Length;
+        }
+
+        return result;
+    }
+}
---
+++ src/Day8/Day8/MapEntry.cs
@@ -0,0 +1,6 @@
+﻿namespace Day8;
+
+public record MapEntry(string Code, string Right, string Left)
+{
+    
+}
