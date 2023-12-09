58092ba158321de06a43c5d91d8a3bce9c53600d
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-09 11:56:38 +0100

    Day8

---
+++ src/Day8/Day8/Map.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+
+namespace Day8;
+
+public class Map
+{
+    private readonly Dictionary<string, MapEntry> _mapEntries;
+    public Map(IEnumerable<MapEntry> mapEntries)
+    {
+        _mapEntries = mapEntries.ToDictionary(item => item.Code, item => item);
+    }
+
+    public int CountMoves(string start, string end, string moves)
+    {
+        var result = 0;
+        var move = 0;
+        var mapEntry = _mapEntries[start];
+
+        while (mapEntry.Code != end)
+        {
+            var direction = moves[move];
+            mapEntry = direction == 'R' ? _mapEntries[mapEntry.Right] : _mapEntries[mapEntry.Left];
+            result++;
+            move++;
+            move %= moves.Length;
+        }
+
+        return result;
+    }
+    public long CountMultipleMoves(char start, char end, string moves)
+    {
+        var mapEntries = _mapEntries.Where(item => item.Key.EndsWith(start)).ToList();
+        var distances = mapEntries.Select(mapEntry => FindLoop(end, moves, mapEntry.Value)).ToArray();
+
+        var lcm = GetLeastCommonMultiple(distances);
+
+        return lcm;
+    }
+
+    private long FindLoop(char end, string moves, MapEntry mapEntry)
+    {
+        long result = 0;
+        var move = 0;
+
+        while (!mapEntry.Code.EndsWith(end))
+        {
+            var direction = moves[move];
+            mapEntry = direction == 'R' ? _mapEntries[mapEntry.Right] : _mapEntries[mapEntry.Left];
+            result++;
+            move++;
+            move %= moves.Length;
+        }
+        return result;
+    }
+
+    private long GetLeastCommonMultiple(long[] numbers)
+    {
+        var result = numbers[0] * numbers[1] / GetGreatestCommonDivisor(numbers[0], numbers[1]);
+        for (var i = 2; i < numbers.Length; i++)
+        {
+            result = result * numbers[i] / GetGreatestCommonDivisor(result, numbers[i]);
+        }
+
+        return result;
+    }
+    
+    private int GetGreatestCommonDivisor(long one, long other)
+    {
+        return (int)BigInteger.GreatestCommonDivisor(new BigInteger(one), new BigInteger(other));
+    }
+}
---
+++ src/Day8/Day8/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Day8;
+
+const string start = "AAA";
+const string end = "ZZZ";
+
+var inputLines = File.ReadAllLines("input.txt");
+var moves = inputLines[0];
+var mapEntries = CreateMap(inputLines.Skip(2));
+var map = new Map(mapEntries);
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var result = map.CountMoves(start, end, moves);
+
+stopWatch.Stop();
+
+Console.WriteLine($"first - {result} - {stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+
+
+stopWatch.Reset();
+stopWatch.Start();
+var multipleResult = map.CountMultipleMoves('A', 'Z', moves);
+Console.WriteLine($"second - {multipleResult} - {stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+stopWatch.Stop();
+
+return ;
+
+IEnumerable<MapEntry> CreateMap(IEnumerable<string> lines)
+{
+    foreach (var line in lines)
+    {
+        var split = line.Split(" = ");
+        var code = split[0];
+        var destinations = split[1].Replace("(", string.Empty).Replace(",", string.Empty).Replace(")", string.Empty).Split(" ");
+        yield return new MapEntry(code, destinations[1], destinations[0]);
+    }
+}
