897414d1f3d1e4b670ff9072d18f109452361e31
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-11 17:20:09 +0100

    Day10

---
+++ src/Day11/Galaxy.cs
@@ -0,0 +1,37 @@
+﻿namespace Day11;
+
+public class Galaxy
+{
+    private const int ExtraValue = 10;
+    private int PosX { get; }
+    private int PosY { get; }
+
+    public Galaxy(int posX, int posY)
+    {
+        PosX = posX;
+        PosY = posY;
+    }
+
+    public int GetDifference(Galaxy other, Dictionary<int, int> extraRows, Dictionary<int, int> extraColumns)
+    {
+        var extraWidth = Math.Abs(extraColumns[other.PosX] - extraColumns[PosX]);
+        var extraHeight = Math.Abs(extraRows[other.PosY] - extraRows[PosY]);
+        var height =extraHeight + Math.Abs(other.PosX - PosX);
+        var width = extraWidth + Math.Abs(other.PosY - PosY);
+        return height + width;
+    }
+    
+    public long GetDifference(Galaxy other, Dictionary<int, int> extraRows, Dictionary<int, int> extraColumns, long extraValue)
+    {
+        if (extraValue == 1)
+        {
+            return GetDifference(other, extraRows, extraColumns);
+        }
+        
+        var extraWidth = Math.Abs(extraColumns[other.PosX] - extraColumns[PosX]);
+        var extraHeight = Math.Abs(extraRows[other.PosY] - extraRows[PosY]);
+        var width = Math.Abs(other.PosX - PosX) - extraWidth;
+        var height = Math.Abs(other.PosY - PosY) - extraHeight;
+        return height + width + extraWidth * extraValue + extraHeight  * extraValue;
+    }
+}
---
+++ src/Day11/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Day11;
+
+var inputLines = File.ReadAllLines("input.txt");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+var extraColumns = new Dictionary<int, int>();
+var extraRows = new Dictionary<int, int>();
+var galaxies = CreateGalaxies(inputLines).ToArray();
+long result = CountAllDifferences(1);
+
+stopWatch.Stop();
+Console.WriteLine($"first - {result} - {stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+stopWatch.Restart();
+
+result = CountAllDifferences(1000000);
+Console.WriteLine($"first - {result} - {stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+
+return;
+
+
+IEnumerable<Galaxy> CreateGalaxies(string[] lines)
+{
+    extraColumns = new Dictionary<int, int>();
+    var extraColumn = 0;
+    for (var column = 0; column < lines[0].Length; column++)
+    {
+        if (lines.Any(row => row[column] == '#'))
+        {
+            extraColumns[column] = extraColumn;
+        }
+        else
+        {
+            extraColumn++;
+            extraColumns[column] = extraColumn;
+        }
+    }
+
+    var extraLine = 0;
+    for (var row = 0; row < lines.Length; row++)
+    {
+        var line = lines[row];
+        if (line.All(item => item == '.'))
+        {
+            extraLine++;
+            extraRows[row] = extraLine;
+            continue;
+        }
+
+        extraRows[row] = extraLine;
+
+        for (var column = 0; column < line.Length; column++)
+        {
+            if (line[column] == '#')
+            {
+                yield return new Galaxy(column, row);
+            }
+        }
+    }
+}
+
+long CountAllDifferences(int extraValue)
+{
+    long res = 0;
+    for (var i = 0; i < galaxies.Length - 1; i++)
+    {
+        var currentGalaxy = galaxies[i];
+        for (var j = i + 1; j < galaxies.Length; j++)
+        {
+            res += currentGalaxy.GetDifference(galaxies[j], extraRows, extraColumns, extraValue);
+        }
+    }
+
+    return res;
+}
