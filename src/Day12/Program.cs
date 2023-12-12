a5bf324936bb316a2ccf9ec17174eb0203a22f1a
Gergely Kucsera <gergely.kucsera@playngo.com>
2023-12-12 15:16:01 +0100

    Day12

---
+++ src/Day12/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Day12;
+
+var inputLines = File.ReadAllLines("input.txt");
+var springSets = CreateSpringSets().ToList();
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var result = springSets.Select(item => item.NumberOfValidSets2(false));
+
+stopWatch.Stop();
+Console.WriteLine($"first - {result.Sum()} - {stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+
+stopWatch.Reset();
+stopWatch.Start();
+
+var setResults = new ConcurrentBag<long>();
+Parallel.ForEach(springSets, set =>
+{
+    var setResult = set.NumberOfValidSets2(true);
+    setResults.Add(setResult);
+});
+
+Console.WriteLine($"second - {setResults.Sum()} - {stopWatch.Elapsed.Minutes}:{stopWatch.Elapsed.Seconds}:{stopWatch.Elapsed.Milliseconds}");
+return ;
+
+
+IEnumerable<SpringSet> CreateSpringSets()
+{
+    foreach (var line in inputLines)
+    {
+        var split = line.Split(" ");
+        var springs = split[1].Split(",").Select(int.Parse).ToArray();
+        yield return new SpringSet(split[0], springs);
+    }
+}
---
+++ src/Day12/SpringSet.cs
@@ -0,0 +1,105 @@
+﻿namespace Day12
+{
+    public class SpringSet
+    {
+        private readonly string _line;
+        private readonly int[] _springs;
+
+        private const char Spring = '#';
+        private const char Blank = '.';
+        private const char Replace = '?';
+
+        public SpringSet(string line, int[] springs)
+        {
+            _line = line;
+            _springs = springs;
+        }
+
+        public long NumberOfValidSets2(bool isUnfolded)
+        {
+            var characters = isUnfolded ? Unfolded : _line;
+            var adjacents = isUnfolded ? UnfoldedSprings().ToArray() : _springs;
+
+            var result = DoCheck(characters.ToCharArray(), 0, adjacents, 0, 0, false, []);
+
+            return result;
+        }
+
+
+        private long DoCheck(char[] line, int index, int[] adjacents, int currentAdjacentIndex, int currentAdjacent, bool isPreviousSequence,
+            Dictionary<(int, int, int, bool), long> cache)
+        {
+            var cacheKey = (index, currentAdjacentIndex, currentAdjacent, isPreviousSequence);
+
+            if (cache.TryGetValue(cacheKey, out var cacheValue)) return cacheValue;
+
+            if (index == line.Length)
+            {
+                if ((currentAdjacentIndex == adjacents.Length && currentAdjacent == 0) ||
+                    (currentAdjacentIndex == adjacents.Length - 1 && adjacents[currentAdjacentIndex] == currentAdjacent)) return 1;
+                return 0;
+            }
+
+            if (line[index] == Blank) return HandleBlank(line, index, adjacents, currentAdjacentIndex, currentAdjacent, isPreviousSequence, cache);
+
+            if (line[index] == Spring)
+            {
+                var result = HandleSpring(line, index, adjacents, currentAdjacentIndex, currentAdjacent, isPreviousSequence, cache);
+                cache[cacheKey] = result;
+                return result;
+            }
+
+            if (line[index] == Replace)
+            {
+                line[index] = Spring;
+                var result = HandleSpring(line, index, adjacents, currentAdjacentIndex, currentAdjacent, isPreviousSequence, cache);
+                line[index] = Blank;
+                result += HandleBlank(line, index, adjacents, currentAdjacentIndex, currentAdjacent, isPreviousSequence, cache);
+                line[index] = Replace;
+                cache[cacheKey] = result;
+                return result;
+            }
+
+            throw new Exception("failed brackets");
+        }
+
+        private long HandleSpring(char[] line, int index, int[] adjacents, int currentAdjacentIndex, int currentAdjacent, bool isPreviousSequence,
+            Dictionary<(int, int, int, bool), long> cache)
+        {
+            if (isPreviousSequence)
+            {
+                currentAdjacent++;
+                if (currentAdjacentIndex < adjacents.Length && currentAdjacent <= adjacents[currentAdjacentIndex])
+                    return DoCheck(line, index + 1, adjacents, currentAdjacentIndex, currentAdjacent, true, cache);
+                return 0;
+            }
+
+            if (currentAdjacentIndex >= adjacents.Length) return 0;
+
+            return DoCheck(line, index + 1, adjacents, currentAdjacentIndex, 1, true, cache);
+        }
+
+        private long HandleBlank(char[] line, int index, int[] adjacents, int currentAdjacentIndex, int currentAdjacent, bool isPreviousSequence,
+            Dictionary<(int, int, int, bool), long> cache)
+        {
+            if (isPreviousSequence)
+            {
+                currentAdjacentIndex++;
+                if (adjacents[currentAdjacentIndex - 1] != currentAdjacent) return 0L;
+
+                return DoCheck(line, index + 1, adjacents, currentAdjacentIndex, 0, false, cache);
+            }
+
+            return DoCheck(line, index + 1, adjacents, currentAdjacentIndex, currentAdjacent, false, cache);
+        }
+
+        public string Unfolded => $"{_line}?{_line}?{_line}?{_line}?{_line}";
+
+        public IEnumerable<int> UnfoldedSprings()
+        {
+            for (var i = 0; i < 5; i++)
+                foreach (var spring in _springs)
+                    yield return spring;
+        }
+    }
+}
